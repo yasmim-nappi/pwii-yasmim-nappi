35d693dbffca55ba477fd9206e1b5457d846dd52
yasmim-nappi <martins.k.nappi@gmail.com>
2025-02-19 10:35:28 -0300

    Aula de introdução a PHP.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pwii-yasmim-nappi
-Programação Web II por Prof. João Siles e Najara Cardoso
+Programação Web II por Prof. João Siles e Najara Cardoso.
---
+++ aula 01/olamundo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <style> 
+        body{
+            background-color: #2b2a3b;
+        }
+        .texto{
+            color: #f2dce9;
+            size: 13rem;
+            text-align: center;
+        }
+    </style>
+<body>
+    <p class= texto>
+<?php
+
+echo "Olá mundo?";
+?>
+    </p>
+</body>
+</html>
