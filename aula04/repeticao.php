53faae80bffebec0431abf746c707515d7bb91d8
Yasmim <martins.k.nappi@gmail.com>
2025-03-19 10:33:24 -0300

    Estruturas de repetição em PHP.

---
+++ aula04/repeticao.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Repetição</title>
+</head>
+<body>
+<?php
+    $i = 1;
+
+    while ($i < 1) {
+       echo $i;
+       $i++;
+                    } 
+
+$i = 1;
+do {
+echo $i;
+$i++;
+} while ($i < 6);
+
+for ($x = 0; $x <= 10; $x++) {
+  echo "O número é: $x <br>";
+}
+
+?>
+</body>
+</html>
