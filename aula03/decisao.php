f19ba62e84753c19a3f7fa5814992ab092f6da92
Yasmim <martins.k.nappi@gmail.com>
2025-06-23 00:40:17 -0300

    Variáveis e Estrutura de Decisão em PHP.

---
+++ aula03/decisao.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .texto{
+            color:pink;
+            background-color: white;
+            }
+    </style>
+    <body>
+    <p class="texto">
+        <?php 
+        $n = 6;
+
+        if ($n >= 4 ) {
+            echo $n, " é maior ou igual a 4 + uma aleração quase plausivel";
+        }
+        $n1 = 3;
+        $n2 = 4;
+        
+        $result = ($n1%$n2)%2;
+
+        if ($result == 1) {
+            echo "<br>" , $result, "quociente ímpar de novo" ;
+        } else {
+            echo "<br>", $result, "quociente par";
+        }
+        ?>
+    </p>
+    </body>
+</html>
---
+++ aula03/variaveis.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .texto{
+            color:pink;
+            background-color: white;
+            }
+    </style>
+    <body>
+    <p class="texto">
+        <?php 
+        $texto = "isso é um texto";
+        $n1 = 2;
+        $n2 =5;
+        $valor = null;
+
+        echo $texto;
+        echo "<br>";
+        echo gettype($texto), "<br>";
+        echo gettype($n1), "<br>"; 
+        echo gettype($n2), "<br>";
+        echo gettype($valor),"<br>";
+        ?>
+    </p>
+    </body>
+</html>
